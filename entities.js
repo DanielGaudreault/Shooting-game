15b16353e9ee132fbbe47c04f1438a53aaa23d97
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2025-05-01 22:37:34 -0400

    Update entities.js

--- entities.js
+++ entities.js
@@ -1,4 +1,4 @@
-class Enemy {
+class Entity {
     constructor(x, y, radius, color, velocity) {
         this.x = x;
         this.y = y;
@@ -18,28 +18,32 @@ class Enemy {
     update() {
         this.x += this.velocity.x;
         this.y += this.velocity.y;
+    }
+
+    isOutOfBounds(canvas) {
+        return (
+            this.x + this.radius < 0 ||
+            this.y + this.radius < 0 ||
+            this.x - this.radius > canvas.width ||
+            this.y - this.radius > canvas.height
+        );
     }
 }
 
-class Projectile {
+export class Player extends Entity {
+    constructor(x, y, radius, color) {
+        super(x, y, radius, color, { x: 0, y: 0 });
+    }
+}
+
+export class Projectile extends Entity {
     constructor(x, y, radius, color, velocity) {
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.color = color;
-        this.velocity = velocity;
+        super(x, y, radius, color, velocity);
     }
+}
 
-    draw(ctx) {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        ctx.fillStyle = this.color;
-        ctx.fill();
-        ctx.closePath();
-    }
-
-    update() {
-        this.x += this.velocity.x;
-        this.y += this.velocity.y;
+export class Enemy extends Entity {
+    constructor(x, y, radius, color, velocity) {
+        super(x, y, radius, color, velocity);
     }
 }
